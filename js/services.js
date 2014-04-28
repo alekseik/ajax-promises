ef1ed1fa776ac453d34cf89ba240231ed2b24469
Dmitry Lesnoy <Dmitry_Lesnoy@epam.com>
2014-04-28 14:49:22 +0300

    format code

--- js/services.js
+++ js/services.js
@@ -3,75 +3,75 @@
 var techtalkServices = angular.module('techtalkServices',['ngResource']);
 
 techtalkServices.factory('Techtalk', ['$http', '$q', function( $http, $q ) {
-    return {
-       getTechtalks: getTechtalks,
-       getLector: getLector,
-       addTechtalk: addTechtalk,
-       readTechtalk: readTechtalk,
-       updateTechtalk: updateTechtalk,
-       deleteTechtalk: deleteTechtalk
-    }
     
-    function getTechtalks() {
-        var request = $http({
-            method: "get",
-            url: "http://54.72.3.96:3000/techtalks"
-        });
+    var getTechtalks = function() {
+            var request = $http({
+                method: "get",
+                url: "http://54.72.3.96:3000/techtalks"
+            });
+            return(request.then(handleSuccess, handleError));
+        },
+        
+        getLector = function(name) {
+            var request = $http({
+                method: "get",
+                url: "http://54.72.3.96:3000/attendees/" + name
+            });
+            return(request.then(handleSuccess, handleError));
+        },
+    
+        addTechtalk = function(data) {
+            var request = $http({
+                method: "post",
+                url: "http://54.72.3.96:3000/techtalks",
+                data: data
+            });
+            return(request.then(handleSuccess, handleError));
+        }, 
+    
+        readTechtalk = function(id) {
+            var request = $http({
+                method: "get",
+                url: "http://54.72.3.96:3000/techtalks/" + id
+            });
+            return(request.then( handleSuccess, handleError));
+        }, 
+    
+        updateTechtalk = function(id, data) {
+            var request = $http({
+                method: "put",
+                url: "http://54.72.3.96:3000/techtalks/" + id,
+                data: data
+            });
+            return(request.then(handleSuccess, handleError));
+        },
+    
+        deleteTechtalk = function(id) {
+            var request = $http({
+                method: "delete",
+                url: "http://54.72.3.96:3000/techtalks/" + id
+            });
+            return(request.then(handleSuccess, handleError));
+        },
+    
+        handleError = function(response) {
+            if (! angular.isObject( response.data ) || ! response.data.message) {
+               return( $q.reject( "An unknown error occurred." ) );
+            }
+            return($q.reject(response.data.message));
 
-        return( request.then( handleSuccess, handleError ) );
-    }
-    
-    function getLector(name) {
-        var request = $http({
-            method: "get",
-            url: "http://54.72.3.96:3000/attendees/" + name
-        });
-        return( request.then( handleSuccess, handleError ) );
-    }
-    
-    function addTechtalk(data) {
-        var request = $http({
-            method: "post",
-            url: "http://54.72.3.96:3000/techtalks",
-            data: data
-        });
-        return( request.then( handleSuccess, handleError ) );
-    }
-    
-    function readTechtalk(id) {
-        var request = $http({
-            method: "get",
-            url: "http://54.72.3.96:3000/techtalks/" + id
-        });
-        return( request.then( handleSuccess, handleError ) );
-    }
-    
-    function updateTechtalk(id, data) {
-        var request = $http({
-            method: "put",
-            url: "http://54.72.3.96:3000/techtalks/" + id,
-            data: data
-        });
-        return( request.then( handleSuccess, handleError ) );
-    }
-    
-    function deleteTechtalk(id) {
-        var request = $http({
-            method: "delete",
-            url: "http://54.72.3.96:3000/techtalks/" + id
-        });
-        return( request.then( handleSuccess, handleError ) );
-    }
-    
-    function handleError( response ) {
-        if (! angular.isObject( response.data ) || ! response.data.message) {
-           return( $q.reject( "An unknown error occurred." ) );
-        }
-        return( $q.reject( response.data.message ) );
+        },
 
-    }
-
-    function handleSuccess( response ) {
-        return( response.data );
-    }
+        handleSuccess = function(response) {
+            return(response.data);
+        };
+        
+        return {
+            getTechtalks: getTechtalks,
+            getLector: getLector,
+            addTechtalk: addTechtalk,
+            readTechtalk: readTechtalk,
+            updateTechtalk: updateTechtalk,
+            deleteTechtalk: deleteTechtalk
+        };
 }]);
